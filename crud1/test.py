aaf8f81f589dcaca298ae1b80732b8d779c9d5b3
Th3f1x <guirafael0909@gmail.com>
2023-08-22 20:23:40 -0300

    Last att. 22/08/23

--- crud1/test.py
+++ crud1/test.py
@@ -1,28 +1,35 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import os
-import sqlite3
+
+
+###Grupo: Guilherme Rafael, Safira Gelli, Kaun Ryo Okimoto
+###Trabalho: LTP3 CRUD
+###22/08/2023           
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
-    descr = db.Column(db.Text)
+    descri = db.Column(db.Text)
     preco = db.Column(db.Text)
     codigo = db.Column(db.Text)
 
-    def __init__(self, nome, descr, preco, codigo):
+    def __init__(self, nome, descri, preco, codigo):
         self.nome = nome
-        self.descr = descr
+        self.descri = descri
         self.preco = preco
-        sel.codigo = codigo
+        self.codigo = codigo
+
 
 @app.route('/')
 def index():
-    objetos = Item.query.all()
-    return render_template('index.html', obejetos=objetos)
+    objet = Item.query.all()
+    return render_template('index.html', objet=objet)
+
 
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
@@ -31,16 +38,18 @@ def registro():
         descri = request.form['descri']
         preco = request.form['preco']
         codigo = request.form['codigo']
-        item = Item(nome=nome, descri=descri, preco=preco,codigo=codigo)
+        item = Item(nome=nome, descri=descri, preco=preco, codigo=codigo)
         db.create_all()
         db.session.add(item)
-        db.session.comit()
+        db.session.commit()
         return redirect('/')
-    return render_template('registro.html')
+    return render_template('registra.html')
 
-@app.route('/edita/<int:item_id>', methods=['GET','POST'])
+
+@app.route('/edita/<int:item_id>', methods=['GET', 'POST'])
 def edita(item_id):
     item = Item.query.get(item_id)
+
     if request.method == 'POST':
         item.nome = request.form['nome']
         item.descri = request.form['descri']
@@ -48,17 +57,20 @@ def edita(item_id):
         item.codigo = request.form['codigo']
         db.session.commit()
         return redirect('/')
+    
     return render_template('edita.html', item=item)
 
-@app.route('/deleta/<int:item_id>', methods=['GET','POST'])
+
+@app.route('/deleta/<int:item_id>', methods=['GET', 'POST'])
 def deleta(item_id):
-    item = Item.query.get(item)
+    item = Item.query.get(item_id)
     if request.method == 'POST':
         db.session.delete(item)
         db.session.commit()
         return redirect('/')
     return render_template('deleta.html', item=item)
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+    
